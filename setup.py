9b21a929006b707343bf528b6bd4445deb4f7e1b
siteisleri <eyupcol@gmail.com>
2021-07-01 17:06:57 +0300

    update on sicaptcha.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="sicaptcha",
-    version="0.0.5",
+    version="0.0.6",
     author="siteisleri.com",
     author_email="noreply@siteisleri.com",
     description="Python basic captcha module",
--- src/sicaptcha/sicaptcha.py
+++ src/sicaptcha/sicaptcha.py
@@ -3,53 +3,56 @@ from io import BytesIO
 import random
 import uuid
 import base64
+import os
 
-captchaSettings = {
-    'bgcolor': (255, 255, 255, 255),
-    'textcolor': (0, 0, 0, 250),
-    'linecolor': (0, 0, 0, 90),
-    'font': 'CaniSketch-Regular.ttf',
-    'fontSize': 48,
-    'format':'base64', #options: base64|image|buffer
-    'imagepath':'captcha.webp',
-    'text':'',
-}
 
-def get_captcha(captchaSettings):
-    textFont = ImageFont.truetype(captchaSettings['font'], captchaSettings['fontSize'])
-    background = Image.new("RGBA", (180, 60), captchaSettings['bgcolor'])
-    w, h = background.size
-    draw = ImageDraw.Draw(background)
+class Sicaptcha:
+    bgcolor = (255, 255, 255, 255)
+    textcolor = (0, 0, 0, 250)
+    linecolor = (0, 0, 0, 90)
+    font = ""
+    fontSize = 48
+    format = 'base64' #options: base64|image|buffer
+    imagepath = 'captcha.webp'
+    text = ''
 
-    points = []
-    for i in range(10):
-        points.append((random.randrange(165) + 10, random.randrange(45) + 10));
+    def get_captcha(self):
+        if not os.path.isfile(self.font) or self.font == "":
+            textFont = ImageFont.load_default()
+        else:
+            textFont = ImageFont.truetype(self.font, self.fontSize)
 
-    draw.line(points, width=5, fill=captchaSettings['linecolor'], joint="curve")
+        background = Image.new("RGBA", (180, 60), self.bgcolor)
+        w, h = background.size
+        draw = ImageDraw.Draw(background)
 
-    step = 10
-    for x in range(step, w, step):
-        for y in range(step, h, step):
-            draw.point((x, y), fill=captchaSettings['textcolor'])
+        points = []
+        for i in range(10):
+            points.append((random.randrange(165) + 10, random.randrange(45) + 10));
 
-    uidStr = str(uuid.uuid4()).split('-')
-    captchaSettings['text'] = uidStr[1]
-    draw1 = ImageDraw.Draw(background)
-    textwidth, textheight = draw1.textsize(captchaSettings['text'], textFont)
-    tx = (180 - textwidth) / 2
-    ty = (60 - textheight) / 2
-    draw1.text((int(tx), int(ty)), captchaSettings['text'], font=textFont, fill=captchaSettings['textcolor'])
+        draw.line(points, width=5, fill=self.linecolor, joint="curve")
 
-    if captchaSettings['format'] == 'base64':
-        buffered = BytesIO()
-        background.save(buffered, format="WEBP")
-        img_str = base64.b64encode(buffered.getvalue())
-        return img_str.decode('utf-8')
-    elif captchaSettings['format'] == 'image':
-        background.save(captchaSettings['imagepath'],'WEBP')
-    elif captchaSettings['format'] == 'buffer':
-        buffered = BytesIO()
-        background.save(buffered, format="WEBP")
-        return buffered.getvalue()
-    else:
-        pass
+        step = 10
+        for x in range(step, w, step):
+            for y in range(step, h, step):
+                draw.point((x, y), fill=self.textcolor)
+
+        uidStr = str(uuid.uuid4()).split('-')
+
+        draw1 = ImageDraw.Draw(background)
+        textwidth, textheight = draw1.textsize(uidStr[1], textFont)
+        tx = (180 - textwidth) / 2
+        ty = (60 - textheight) / 2
+        draw1.text((int(tx), int(ty)), uidStr[1], font=textFont, fill=self.textcolor)
+
+        if self.format == 'base64':
+            buffered = BytesIO()
+            background.save(buffered, format="WEBP")
+            img_str = base64.b64encode(buffered.getvalue())
+            return img_str
+        elif self.format == 'image':
+            background.save(self.imagepath, 'WEBP')
+        else:
+            buffered = BytesIO()
+            background.save(buffered, format="WEBP")
+            return buffered.getvalue()
